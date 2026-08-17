60a9512bf7466a536568a5c3024ec5a9605e8931
Aryabardhansinghchauhan <aryabardhansinghchauhan@gmail.com>
2026-08-17 14:33:41 +0530

    Refactor CORS middleware for improved origin handling

--- interview-ai-yt-main/Backend/src/app.js
+++ interview-ai-yt-main/Backend/src/app.js
@@ -4,19 +4,21 @@ const cors = require("cors")
 
 const app = express()
 
+
 app.use(express.json())
 app.use(cookieParser())
+
 
 const allowedOrigins = [
     "http://localhost:5173",
     "https://ai-interview-preparation-web-page.vercel.app"
 ]
 
+
 app.use(
     cors({
         origin: function (origin, callback) {
-
-           
+          
             if (!origin) {
                 return callback(null, true)
             }
@@ -27,11 +29,11 @@ app.use(
 
             return callback(new Error("Not allowed by CORS"))
         },
-
         credentials: true
     })
 )
 
+// Routes
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
