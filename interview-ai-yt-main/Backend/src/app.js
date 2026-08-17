f193299391ae7774011c4ca8ebee6135438aa5fe
Aryabardhansinghchauhan <aryabardhansinghchauhan@gmail.com>
2026-08-17 14:31:06 +0530

    Update CORS settings to support multiple origins
    Refactor CORS configuration to allow multiple origins dynamically.

--- interview-ai-yt-main/Backend/src/app.js
+++ interview-ai-yt-main/Backend/src/app.js
@@ -6,20 +6,37 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "https://ai-interview-preparation-web-page.vercel.app",
-    credentials: true
-}))
 
-/* require all the routes here */
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ai-interview-preparation-web-page.vercel.app"
+]
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+
+            // Allow requests with no origin
+            // Example: Postman/server-to-server
+            if (!origin) {
+                return callback(null, true)
+            }
+
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true)
+            }
+
+            return callback(new Error("Not allowed by CORS"))
+        },
+
+        credentials: true
+    })
+)
+
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
-/* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
-
 
 module.exports = app
